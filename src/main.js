e8b021750684ddf572013a4dbfe90921432c3939
xiedongwei <2829527458@qq.com>
2023-03-06 01:46:59 +0800

    登录注册页面

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './mock/index.js'
+Vue.config.productionTip = false;
 Vue.use(VueAxios, axios)
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="login">
-    <h1>111</h1>
+  <div class="form">
+    <h1>登录</h1>
+    <a-form id="components-form-demo-normal-login" :form="form" class="login-form" @submit="handleSubmit">
+      <a-form-item>
+        <a-input placeholder="Username">
+          <a-icon slot="prefix" type="user" style="color: rgba(0, 0, 0, 0.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input type="password" placeholder="Password">
+          <a-icon slot="prefix" type="lock" style="color: rgba(0, 0, 0, 0.25)" />
+        </a-input>
+      </a-form-item>
+      <div class="confim">
+        <a-button type="primary" html-type="submit" class="login-form-button"> Log in </a-button>
+        <a-button type="primary" html-type="submit" class="login-form-button"> regsiter</a-button>
+      </div>
+    </a-form>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 export default {
@@ -29,7 +44,7 @@ export default {
     regsiter() {
       var userinfo = {
         name: '555',
-        password: '555'
+        password: '444'
       }
       this.axios
         .post('/api/register', userinfo)
@@ -43,4 +58,29 @@ export default {
   }
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 30%;
+}
+.form {
+  height: 350px;
+  width: 400px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .confim {
+    display: flex;
+    justify-content: space-between;
+  }
+  h1 {
+    text-align: center;
+  }
+}
+</style>
