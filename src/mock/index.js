6c18a4d32f79ee7695ad438f750169ae3f01ff8b
xiedongwei <2829527458@qq.com>
2023-03-06 00:10:15 +0800

    登录注册mock

---
+++ src/mock/index.js
@@ -0,0 +1,65 @@
+import Mock from 'mockjs'
+var userdata = [{ name: '111', password: '111' }, { name: '222', password: '222' }, { name: '333', password: '333' }]
+//注册路由token验证
+Mock.mock('/user/userInfo', 'post', (req) => {
+  var tokeninfo = ''
+  var userinfo = JSON.parse(req.body)
+  userdata.forEach(item => {
+    if (item.name == userinfo.name && item.password == userinfo.password)
+      tokeninfo = 'xiedongwei'
+  })
+  return {
+    code: 200,
+    token: tokeninfo,
+    message: '查询用户信息成功',
+    data: userdata
+  }
+})
+//注册用户
+Mock.mock('/api/register', 'post', (req) => {
+  var msg = ''
+  var userinfo = JSON.parse(req.body)
+  try {
+    userdata.forEach(item => {
+      if (item.name == userinfo.name) {
+        userdata.forEach(item => {
+          if (item.name == userinfo.name) {
+            throw new Error("该账户已存在");
+          }
+        })
+      }
+    })
+
+  } catch (e) {
+    msg = '该账户已存在'
+  }
+  if (msg == '') {
+    userdata.push(userinfo)
+    msg = '注册成功请登录'
+  }
+
+  console.log(userdata)
+  return {
+    code: 200,
+    message: msg,
+  }
+})
+var { newList } = Mock.mock({
+  'newList|20-30': [
+    {
+      id: '@increment(1)',
+      title: '@ctitle',
+      updat: '@date(yyyy-MM-dd hh:mm:ss)',
+      info: '@cparagraph(5,10)',
+      small: '@dataImage(300x200,电影图片)'
+    }
+  ]
+})
+Mock.mock('/api/movie', 'get', () => {
+  return {
+    status: 200,
+    message: '获取电影列表成功！',
+    total: newList.length,
+    data: newList
+  }
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login">
+    <h1>111</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  mounted() {
+    this.login()
+    this.regsiter()
+  },
+  methods: {
+    login() {
+      var userinfo = {
+        name: '111',
+        password: '222'
+      }
+      this.axios
+        .post('/user/userInfo', userinfo)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    },
+    regsiter() {
+      var userinfo = {
+        name: '555',
+        password: '555'
+      }
+      this.axios
+        .post('/api/register', userinfo)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+  }
+}
+</script>
+<style lang="scss"></style>
